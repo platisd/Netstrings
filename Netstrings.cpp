126fe1a4d3d348291ce055e7b1c50d69ed9ed076
dimi88 <kurukii88@gmail.com>
2015-03-10 16:15:49 +0100

    changed variable names to make them more indicate and improved the versatility of the decoding method to handle strings that end with comma

--- Netstrings.cpp
+++ Netstrings.cpp
@@ -6,21 +6,22 @@
 
 #include "Netstrings.h"
 
-String encodedNetstring(String input){
-	unsigned int len = input.length();
+String encodedNetstring(String plainInput){
+	unsigned int len = plainInput.length();
 	if (!len) return "error"; //if the input string is empty, return "error"
-	return len + String(":" + input + ","); //return a Netstring in the form of [len]":"[string]","
+	return len + String(":" + plainInput + ","); //return a Netstring in the form of [len]":"[string]","
 }
 
-String decodedNetstring(String input){
-	if (input.length() < 3) return "error"; //if the Netstring is less than 3 characters, it's either an invalid one or contains an empty string
-	int semicolonIndex = input.indexOf(':');
+String decodedNetstring(String netstring){
+	if (netstring.length() < 3) return "error"; //if the Netstring is less than 3 characters, it's either an invalid one or contains an empty string
+	int semicolonIndex = netstring.indexOf(':');
 	if (semicolonIndex < 0) return "error"; // if there's no semicolon, then it's an invalid Netstring
-	String parsedDigits = input.substring(0, semicolonIndex); //parse until the semicolon. Those should be the control digits
+	String parsedDigits = netstring.substring(0, semicolonIndex); //parse until the semicolon. Those should be the control digits
 	int controlDigits = parsedDigits.toInt();
 	if (controlDigits < 1) return "error"; //if the control digit is smaller than 1, then it's either not a digit or the Netstring is invalid
-	String command = input.substring(semicolonIndex+1); //parse after the semicolon until the end of the string
+	String command = netstring.substring(semicolonIndex+1); //parse after the semicolon until the end of the string
 	if (!command.length()) return "error"; // if it's an empty string, return "error"
+	if (command.substring(command.length() -1) == ",") command.remove(command.length()-1); //if last character is a comma, remove it
 	if (command.length() != controlDigits) return "error"; //if string's length isn't equal with the control digits, it's an invalid Netstring
 	return command;
 }
